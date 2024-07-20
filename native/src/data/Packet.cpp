b2bb1ce9c25517df1e686c22a1fa786cd696547e
Silas Della Contrada <me@silenium-dev.net>
2024-07-20 22:40:15 +0200

    feat: add size and data properties to Packet, extend FileDemuxerTest

--- native/src/data/Packet.cpp
+++ native/src/data/Packet.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <jni.h>
+#include <cstring>
 
 extern "C" {
 #include <libavformat/avformat.h>
@@ -14,5 +15,26 @@ JNIEXPORT void JNICALL Java_dev_silenium_multimedia_data_PacketKt_releasePacketN
 ) {
     auto avPacket = reinterpret_cast<AVPacket *>(packet);
     av_packet_free(&avPacket);
+}
+
+JNIEXPORT jint JNICALL Java_dev_silenium_multimedia_data_PacketKt_sizeN(
+    JNIEnv *env,
+    jobject thiz,
+    const jlong packet
+) {
+    const auto avPacket = reinterpret_cast<AVPacket *>(packet);
+    return avPacket->size;
+}
+
+JNIEXPORT jobject JNICALL Java_dev_silenium_multimedia_data_PacketKt_dataN(
+    JNIEnv *env,
+    jobject thiz,
+    const jlong packet,
+    jobject buf 
+) {
+    const auto avPacket = reinterpret_cast<AVPacket *>(packet);
+    const auto bufPtr = env->GetDirectBufferAddress(buf);
+    std::memcpy(bufPtr, avPacket->data, avPacket->size);
+    return buf;
 }
 }
--- src/test/kotlin/dev/silenium/multimedia/demux/FileDemuxerTest.kt
+++ src/test/kotlin/dev/silenium/multimedia/demux/FileDemuxerTest.kt
@@ -1,6 +1,7 @@
 package dev.silenium.multimedia.demux
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.result.shouldBeSuccess
 import io.kotest.matchers.shouldBe
 import java.nio.file.Files
@@ -17,7 +18,10 @@ class FileDemuxerTest : FunSpec({
             videoFile.outputStream().use(it::copyTo)
         }
         val demuxer = FileDemuxer(videoFile)
-        demuxer.nextPacket().shouldBeSuccess()
+        val packet = demuxer.nextPacket().shouldBeSuccess()
+        packet.size shouldBeGreaterThan 0
+        packet.data.limit() shouldBe packet.size
+        packet.close()
         demuxer.close()
     }
 })
