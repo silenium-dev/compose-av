208f7529dd6f128c1b36c93e4b091aa067b49823
silenium-dev <me@silenium-dev.net>
2026-03-05 20:45:19 +0100

    build: working pure nix build for natives

--- native/flake.nix
+++ native/flake.nix
@@ -10,24 +10,36 @@
     let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
     in
-    {
+    rec {
       packages."x86_64-linux" = jni-utils.lib.buildJNILib {
         name = "compose-av";
         mesonTarget = "compose-av";
+        buildType = "release";
         libName = "compose-av";
         libDir = "src";
 
-        additionalNativeInputs = [ pkgs.p7zip ];
+        additionalNativeInputs = [ pkgs.p7zip pkgs.curl pkgs.cacert pkgs.python3 ];
         sources = targetSystem:
           let
-            mpv = import ./mpv-config.nix { inherit pkgs; };
+            mpv = import ./nix/mpv-config.nix { inherit pkgs; };
+            ffmpeg = import ./nix/ffmpeg-config.nix { inherit pkgs; };
           in
-          [
+          (if targetSystem == "x86_64-windows" then
+            [
+              (builtins.fetchurl {
+                url = mpv.${targetSystem}.source_url;
+                sha256 = mpv.${targetSystem}.source_hash;
+                name = "mpv.7z";
+              })
+            ]
+          else [
+            (import ./nix/linux-sources.nix { inherit pkgs; })
             (builtins.fetchurl {
-              url = mpv.${targetSystem}.mpv_source_url;
-              sha256 = mpv.${targetSystem}.mpv_source_hash;
-              name = "mpv.7z";
+              url = ffmpeg.${targetSystem}.source_url;
+              sha256 = ffmpeg.${targetSystem}.source_hash;
+              name = "ffmpeg.tar.xz";
             })
+          ]) ++ [
             (builtins.path {
               path = ./.;
               name = "compose-av";
@@ -42,40 +54,136 @@
             srcName=$(stripHash "$src")
 
             case "$src" in
+              *.tar.gz|*.tar.xz)
+                dirName="''${srcName%.tar.*}"
+                echo "Extracting tar archive into: $dirName"
+                mkdir -p "$dirName"
+                tar xf "$src" -C "$dirName"
+
+                # Check if archive only contained one directory
+                shopt -s nullglob dotglob
+                contents=("$dirName"/*)
+                if [ ''${#contents[@]} -eq 1 ] && [ -d "''${contents[0]}" ]; then
+                  echo "Flattening single directory structure"
+                  mv "''${contents[0]}"/* "$dirName"/
+                  rmdir "''${contents[0]}"
+                fi
+
+                chmod -R +w "$dirName"
+
+                for i in "$dirName"/**/*.py "$dirName"/*.py; do
+                  echo "patching shebangs in $i"
+                  patchShebangs --build "$i"
+                done
+
+                tar cf "$dirName.tar" -C "$dirName" .
+                rm -rf "$dirName"
+                ;;
+              *.zip)
+                dirName="''${srcName%.zip}"
+                echo "Extracting tar archive into: $dirName"
+                mkdir -p "$dirName"
+                7z x -o"$dirName" "$src"
+
+                # Check if archive only contained one directory
+                shopt -s nullglob dotglob
+                contents=("$dirName"/*)
+                if [ ''${#contents[@]} -eq 1 ] && [ -d "''${contents[0]}" ]; then
+                  echo "Flattening single directory structure"
+                  mv "''${contents[0]}"/* "$dirName"/
+                  rmdir "''${contents[0]}"
+                fi
+
+                chmod -R +w "$dirName"
+
+                for i in "$dirName"/**/*.py "$dirName"/*.py; do
+                  echo "patching shebangs in $i"
+                  patchShebangs --build "$i"
+                done
+
+                tar cf "$dirName.tar" -C "$dirName" .
+                rm -rf "$dirName"
+                ;;
               *.7z)
-              dirName="''${srcName%.7z}"
-              echo "Extracting 7z archive into: $dirName"
-              mkdir -p "$dirName"
-              7z x -o"$dirName" "$src"
-              ;;
-            *)
-              echo "Copying: $srcName"
-              cp -r "$src" "$srcName"
-              chmod -R +w "$srcName"
-              ;;
+                dirName="''${srcName%.7z}"
+                echo "Extracting 7z archive into: $dirName"
+                mkdir -p "$dirName"
+                7z x -o"$dirName" "$src"
+
+                chmod -R +w "$dirName"
+                tar cf "$dirName.tar" -C "$dirName" .
+                ;;
+              *compose-av)
+                echo "Copying: $srcName"
+                cp -r "$src" "$srcName"
+                chmod -R +w "$srcName"
+                ;;
+              *)
+                dirName="''${srcName}"
+                echo "Compressing: $srcName"
+                cp -r "$src" "$srcName"
+                chmod -R +w "$srcName"
+
+                for i in "$dirName"/**/*.py "$dirName"/*.py; do
+                  echo "patching shebangs in $i"
+                  patchShebangs --build "$i"
+                done
+
+                tar cf "$dirName.tar" -C "$srcName" .
+                rm -rf "$srcName"
+                ;;
             esac
           done
 
           runHook postUnpack
         '';
-
-        patch = targetSystem: ''
-          ls -la compose-av
+        postUnpackPhase = targetSystem : ''
+          shopt -s globstar
           case "${targetSystem}" in
-            *-linux)
-              echo "todo"
-              exit 1
-              ;;
             *-windows)
-              mkdir -p compose-av/subprojects
-              cp -r mpv compose-av/subprojects/mpv
+              mkdir -p compose-av/subprojects/mpv
+              tar xf mpv.tar -C compose-av/subprojects/mpv/
               cp compose-av/subprojects.tpl/windows/packagefiles/mpv/* compose-av/subprojects/mpv/
+              ;;
+            *-linux)
+              for p in ./*-patch.tar; do
+                echo "patching ''${p%-patch.tar}"
+                tar --concatenate --file="''${p%-patch.tar}.tar" "$p"
+                rm "$p"
+              done
+
+              mkdir -p compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/libass/subprojects/packagefiles
+              mkdir -p compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/libass/subprojects/packagefiles/libfontconfig/subprojects/packagefiles
+              mv libfontconfig.tar compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/libass/subprojects/packagefiles
+              mv libxml2.tar compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/libass/subprojects/packagefiles/libfontconfig/subprojects/packagefiles
+              mv gperf.tar compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/libass/subprojects/packagefiles/libfontconfig/subprojects/packagefiles
+
+              mkdir -p compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/harfbuzz/subprojects/packagefiles
+              mv icu.tar compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles/harfbuzz/subprojects/packagefiles
+
+              mv mpv.tar compose-av/subprojects/packagefiles
+
+              mv *.tar compose-av/subprojects/packagefiles/mpv/subprojects/packagefiles
+
+              for i in compose-av/subprojects.tpl/linux/**/*; do
+                if [ -d "$i" ]; then
+                  echo "creating dir $i"
+                  mkdir -p "compose-av/subprojects/''${i#compose-av/subprojects.tpl/linux/}"
+                else
+                  echo "Expanding $i:"
+                  cat "$i" \
+                    | sed 's,''${cav_archive_base},'"$(pwd)"',g' \
+                    > "compose-av/subprojects/''${i#compose-av/subprojects.tpl/linux/}"
+                fi
+              done
               ;;
           esac
 
-          sourceRoot="compose-av"
-          cd $sourceRoot
+          sourceRoot="$(pwd)/compose-av"
+          echo "changing source root: $sourceRoot"
+          cd "$sourceRoot"
         '';
       };
+      checks = packages;
     };
 }
