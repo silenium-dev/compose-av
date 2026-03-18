f737c8a98f20670a5bc344bd4a18073c5f1b9c5a
silenium-dev <me@silenium-dev.net>
2026-03-18 21:22:57 +0100

    chore: move to nexus

--- natives/nix/mpv-config.nix
+++ natives/nix/mpv-config.nix
@@ -2,7 +2,7 @@
 }:
 let
   mpv = { arch, hash }: rec {
-    source_url = "https://repoflow.silenium.dev/api/universal/personal/github-releases/shinchiro/mpv-winbuild-cmake/20260225/mpv-dev-${arch}-20260225-git-92ed2d2.7z";
+    source_url = "https://nexus.silenium.dev/repository/github-releases/shinchiro/mpv-winbuild-cmake/releases/download/20260307/mpv-dev-${arch}-20260307-git-f9190e5.7z";
     source_hash = hash;
     source_filename = pkgs.lib.lists.last (pkgs.lib.strings.split "/" source_url);
     directory = builtins.elemAt (pkgs.lib.strings.split "." source_filename) 0;
@@ -13,7 +13,7 @@ in
     arch = "x86_64";
     hash = "sha256-XSZqaJm4uxdaaFfJPFNnnwQJUxZ6K+1luD7bA+C0i2U=";
   };
-  "aarch64-windows" = {
+  "aarch64-windows" = mpv {
     arch = "aarch64";
     hash = "sha256-eDtgyMqU7adllvKzVfhg813/5vVqouN2Ly/H/T9zBNI=";
   };
