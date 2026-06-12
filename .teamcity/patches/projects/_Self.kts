2d930bd80dbbb57acfed00a66147d85910ae739a
silenium-dev <silenium-dev@TeamCity>
2026-06-12 09:39:31 +0000

    commit current project settings (TeamCity change in 'Public / compose-av' project)

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -11,6 +11,11 @@ To apply the patch, change the root project
 accordingly, and delete the patch script.
 */
 changeProject(DslContext.projectId) {
+    check(archived == false) {
+        "Unexpected archived: '$archived'"
+    }
+    archived = true
+
     features {
         remove {
             githubConnection {
